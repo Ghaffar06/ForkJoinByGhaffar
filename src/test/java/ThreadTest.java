9f6b67c214cbf6b368f571446a00aa2bb65dadf8
Mohamad Bashar Desoki <mhdbashard@gmail.com>
2023-03-07 10:42:47 +0300

    03. Save Record using Runnable

--- src/test/java/ThreadTest.java
+++ src/test/java/ThreadTest.java
@@ -24,7 +24,7 @@ public class ThreadTest extends TestCase {
     public void testStudentThread() {
         long start = System.currentTimeMillis();
 
-        System.out.println("Name of current Thread: "+Thread.currentThread().getName());
+        System.out.println("Name of current Thread: " + Thread.currentThread().getName());
 
         StudentService studentService = new StudentService();
         Student std = new Student("student");
@@ -42,16 +42,16 @@ public class ThreadTest extends TestCase {
         System.out.println("Execution Time: " + execTime + " ms");
     }
 
-    public void testStudentsThreads(){
+    public void testStudentsThreads() {
         long start = System.currentTimeMillis();
 
         StudentService studentService = new StudentService();
 
-        for (int i=1;i<=10;++i){
+        for (int i = 1; i <= 10; ++i) {
 
-            Student std = new Student("student "+i);
+            Student std = new Student("student " + i);
             StudentThread studentThread = new StudentThread(std, studentService);
-            studentThread.setName("myThread-"+i);
+            studentThread.setName("myThread-" + i);
             studentThread.start(); // start thread
             //        studentThread.run(); // treat as a simple object not as a thread
             try {
@@ -64,16 +64,16 @@ public class ThreadTest extends TestCase {
         System.out.println("Execution Time: " + execTime + " ms");
     }
 
-    public void testStudentThreadsPP(){
+    public void testStudentThreadsPP() {
         long start = System.currentTimeMillis();
 
         StudentService studentService = new StudentService();
         List<StudentThread> studentThreads = new ArrayList<>();
-        for (int i=1;i<=10;++i){
+        for (int i = 1; i <= 10; ++i) {
 
-            Student std = new Student("student "+i);
+            Student std = new Student("student " + i);
             StudentThread studentThread = new StudentThread(std, studentService);
-            studentThread.setName("myThread-"+i);
+            studentThread.setName("myThread-" + i);
             studentThread.start(); // start thread
             studentThreads.add(studentThread);
             //        studentThread.run(); // treat as a simple object not as a thread
@@ -88,5 +88,58 @@ public class ThreadTest extends TestCase {
         });
         long execTime = System.currentTimeMillis() - start;
         System.out.println("Execution Time: " + execTime + " ms");
+    }
+
+    public void testStudentRunnable() {
+        StudentService studentService = new StudentService();
+        Student std = new Student("student");
+        StudentRunnable studentRunnable = new StudentRunnable(std, studentService);
+
+        Thread thread = new Thread(studentRunnable);
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Completed Runnable");
+    }
+
+    public void testStudentRunnableAnonymousClass() {
+        StudentService studentService = new StudentService();
+        Student std = new Student("student");
+//        StudentRunnable studentRunnable = new StudentRunnable(std, studentService);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                studentService.saveStudent(std);
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Completed Runnable");
+    }
+
+    public void testStudentRunnableLambda() {
+        StudentService studentService = new StudentService();
+        Student std = new Student("student");
+
+        Thread thread = new Thread(() -> {
+            studentService.saveStudent(std);
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Completed Runnable");
     }
 }
